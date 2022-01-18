1f179b23e269875b94e8587a3bb17ecc7c309dd2
what-s-good <91669707+what-s-good@users.noreply.github.com>
2022-01-18 18:55:48 +0100

    siebter Entwurf

--- daten.py
+++ daten.py
@@ -14,13 +14,30 @@ def speichern(datei, key, pflanze, giessen, wasser, sonstig):
         "Datum": str(key),
         "Giessen": giessen,
         "Wasser": wasser,
-        "Sonstiges:": sonstig
+        "Sonstiges": sonstig
+    })
+
+    with open("pflanzendaten.json", "w") as open_file:
+        json.dump(datei_inhalt, open_file, indent=4)
+
+def aufgabe_speichern(pflanze, wasser, sonstiges, datum):
+    try:
+        with open("aufgaben.json") as open_file:
+            aufgabe_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        aufgabe_inhalt = []
+
+    aufgabe_inhalt.append ({
+        "Pflanze": pflanze,
+        "Datum": str(datum),
+        "Wasser": wasser,
+        "Sonstiges": sonstiges
     })
 
     # print(datei_inhalt)
 
-    with open(datei, "w") as open_file:
-        json.dump(datei_inhalt, open_file, indent=4)
+    with open("aufgaben.json", "w") as open_file:
+        json.dump(aufgabe_inhalt, open_file, indent=4)
 
 
 def aktivitaet_speichern(pflanze, giessen, wasser, sonstig):
@@ -32,6 +49,17 @@ def aktivitaet_speichern(pflanze, giessen, wasser, sonstig):
 
 def pflanzen_laden():
     datei_name = "pflanzendaten.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
+
+def aufgaben_laden():
+    datei_name = "aufgaben.json"
 
     try:
         with open(datei_name) as open_file:
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
 import daten
 from daten import pflanzen_laden
+from daten import aufgaben_laden
+import datetime as dt #wäre nur datetime, hätte es probleme gegeben mit Zeile 9 und 10. Quelle: https://stackoverflow.com/questions/15707532/import-datetime-v-s-from-datetime-import-datetime
 from datetime import datetime
 from datetime import date
 
 app = Flask("Hello World")
 
+#Quelle zum allgemeinen Verständnis (die ganze Serie): https://www.youtube.com/watch?v=GgS8-mn9zoM&list=PLNmsVeXQZj7otfP2zTa8AIiNIWVg0BRqs&index=12
 
 @app.route("/")
 def hello():
@@ -37,19 +41,43 @@ def garten():
     stockladen = pflanzen_laden()
     return render_template('garten.html', stockladen = stockladen)
 
-@app.route("/aufgaben", methods=['GET'])
+@app.route("/aufgaben", methods=['GET', 'POST'])
 def aufgaben():
-    stockaufgaben = []
-    for datum in pflanzen_laden():
+
+    with open("pflanzendaten.json") as open_file:
+        pflanzendaten = json.load(open_file)
+
+    for pflanze in pflanzendaten:
+        if request.method == "POST":
+            if request.form.get(pflanze["Pflanze"]) == "Erledigt":
+                pflanze["Datum"] = str(dt.date.today())
+
+    with open("pflanzendaten.json", "w") as open_file:
+        json.dump(pflanzendaten, open_file, indent=4)
+
+    aufgabe_inhalt = [] #macht leere liste, damit aufgaben nicht mehrmals erscheinen
+    with open("aufgaben.json", "w") as open_file: #gibt leere liste ins json
+        json.dump(aufgabe_inhalt, open_file, indent=4)
+
+    for datum in pflanzen_laden(): #Mit Nicolas Steiger angeschaut
+        # https://stackoverflow.com/questions/8419564/difference-between-two-dates-in-python
         format = "%Y-%m-%d"
         datum1 = datetime.strptime(datum["Datum"], format) #format formatiert dass Datum
         datum_heute = datetime.strptime(str(date.today()), format)
         datumberechnet = datum_heute - datum1
-        print(datumberechnet.days)
+        print(datumberechnet) #0:00:00
+        print(datumberechnet.days) #0
         if datumberechnet.days != 0: #die Pflanzen die heute eingegeben wurden werden nicht angezeigt
-            if datumberechnet.days % int(datum["Giessen"]) == 0:
-                stockaufgaben.append(datum["Pflanze"])
-    return render_template('aufgaben.html', stockaufgaben = stockaufgaben)
+            if datumberechnet.days % int(datum["Giessen"]) == 0: #wenn rest null ergibt
+
+                daten.aufgabe_speichern(datum["Pflanze"], datum["Wasser"], datum["Sonstiges"], datum_heute)
+
+    with open("aufgaben.json") as open_file:
+            aufgabe_inhalt = json.load(open_file)
+
+
+
+    return render_template('aufgaben.html', aufgabe_inhalt=aufgabe_inhalt)
 
 
 if __name__ == "__main__":
